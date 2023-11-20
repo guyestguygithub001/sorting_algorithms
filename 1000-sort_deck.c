37ae602a2ffda76cddfef1d8b993338b30cefa40
Augustine <austynomilano77@gmail.com>
2023-11-20 03:41:08 -0800

    card sort updated

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -97,38 +97,45 @@ void insertion_sort_deck_kind(deck_node_t **deck)
  *                             spades to diamonds from ace to king.
  * @deck: A pointer to the head of a deck_node_t doubly-linked list.
  */
- void insertion_sort_deck_value(deck_node_t **deck)
- {
-	 deck_node_t *iterate, *insert, *temp;
+void insertion_sort_deck_value(deck_node_t **deck)
+{
+	deck_node_t *iterate, *insert, *temp;
 
-	 for (iterate = (*deck)->next; iterate != NULL; iterate = temp)
-	 {
+	for (iterate = (*deck)->next; iterate != NULL; iterate = temp)
+	{
+		temp = iterate->next;
+		insert = iterate->next;
+		while (insert != NULL &&
+			insert->card->kind == iterate->card->kind &&
+			get_value(insert) > get_value(iterate))
+		{
+			insert->next = iterate->next;
+			if (iterate->next != NULL)
+				iterate->next->prev = insert;
+			iterate->prev = insert->prev;
+			iterate->next = insert;
+			if (insert->prev != NULL)
+				insert->prev->next = iterate;
+			else
+				*deck = iterate;
+			insert->prev = iterate;
+			insert = iterate->prev;
+		}
+	}
+}
 
-		 temp = iterate->next;
-		 insert = iterate->next;
-		 while (insert != NULL &&
-			 insert->card->kind == iterate->card->kind &&
-			 get_value(insert) > get_value(iterate))
-		 {
-			 insert->next = iterate->next;
-			 if (iterate->next != NULL)
-				 iterate->next->prev = insert;
-			 iterate->prev = insert->prev;
-			 iterate->next = insert;
-			 if (insert->prev != NULL)
-				 insert->prev->next = iterate;
-			 else
-				 *deck = iterate;
-			 insert->prev = iterate;
-			 insert = iterate->prev;
-		 }
-	 }
- }
- /**
+/**
  * sort_deck - Sort a deck of cards from ace to king and
  *             from spades to diamonds.
  * @deck: A pointer to the head of a deck_node_t doubly-linked list.
+ *
+ * This function sorts the given deck of cards in two steps:
+ * 1. Sorts the cards by their suit (kind) from spades to diamonds.
+ * 2. Within each suit, sorts the cards by their value from ace to king.
+ *
+ * @deck: A pointer to the head of a deck_node_t doubly-linked list.
  */
+
 void sort_deck(deck_node_t **deck)
 {
 	if (deck == NULL || *deck == NULL || (*deck)->next == NULL)
